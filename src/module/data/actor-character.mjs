0aaaed251a0e8c8b5b3cd44e5cfc8cbab3da254b
Jeronimo Rodriguez <jero.rg@gmail.com>
2026-01-08 18:36:56 +0100

    feat: new character sheet header

--- src/module/data/actor-character.mjs
+++ src/module/data/actor-character.mjs
@@ -1,7 +1,6 @@
 import VsDActorBase from "./base-actor.mjs";
 
 export default class VsDCharacter extends VsDActorBase {
-
   static defineSchema() {
     const fields = foundry.data.fields;
     const requiredInteger = { required: true, nullable: false, integer: true };
@@ -9,17 +8,66 @@ export default class VsDCharacter extends VsDActorBase {
 
     schema.attributes = new fields.SchemaField({
       level: new fields.SchemaField({
-        value: new fields.NumberField({ ...requiredInteger, initial: 1 })
+        value: new fields.NumberField({ ...requiredInteger, initial: 1 }),
+      }),
+    });
+
+    schema.details = new fields.SchemaField({
+      kin: new fields.StringField({
+        required: false,
+        nullable: false,
+        initial: "",
+      }),
+      vocation: new fields.StringField({
+        required: false,
+        nullable: false,
+        initial: "",
+      }),
+      culture: new fields.StringField({
+        required: false,
+        nullable: false,
+        initial: "",
+      }),
+      motivation: new fields.StringField({
+        required: false,
+        nullable: false,
+        initial: "",
+      }),
+      nature: new fields.StringField({
+        required: false,
+        nullable: false,
+        initial: "",
+      }),
+      allegiance: new fields.StringField({
+        required: false,
+        nullable: false,
+        initial: "",
+      }),
+    });
+
+    schema.advancement = new fields.SchemaField({
+      xp: new fields.NumberField({
+        required: true,
+        nullable: false,
+        integer: true,
+        initial: 0,
+        min: 0,
       }),
     });
 
     // Iterate over ability names and create a new SchemaField for each.
-    schema.abilities = new fields.SchemaField(Object.keys(CONFIG.VSD.abilities).reduce((obj, ability) => {
-      obj[ability] = new fields.SchemaField({
-        value: new fields.NumberField({ ...requiredInteger, initial: 10, min: 0 }),
-      });
-      return obj;
-    }, {}));
+    schema.abilities = new fields.SchemaField(
+      Object.keys(CONFIG.VSD.abilities).reduce((obj, ability) => {
+        obj[ability] = new fields.SchemaField({
+          value: new fields.NumberField({
+            ...requiredInteger,
+            initial: 10,
+            min: 0,
+          }),
+        });
+        return obj;
+      }, {})
+    );
 
     return schema;
   }
@@ -28,9 +76,12 @@ export default class VsDCharacter extends VsDActorBase {
     // Loop through ability scores, and add their modifiers to our sheet output.
     for (const key in this.abilities) {
       // Calculate the modifier using d20 rules.
-      this.abilities[key].mod = Math.floor((this.abilities[key].value - 10) / 2);
+      this.abilities[key].mod = Math.floor(
+        (this.abilities[key].value - 10) / 2
+      );
       // Handle ability label localization.
-      this.abilities[key].label = game.i18n.localize(CONFIG.VSD.abilities[key]) ?? key;
+      this.abilities[key].label =
+        game.i18n.localize(CONFIG.VSD.abilities[key]) ?? key;
     }
   }
 
@@ -40,13 +91,13 @@ export default class VsDCharacter extends VsDActorBase {
     // Copy the ability scores to the top level, so that rolls can use
     // formulas like `@str.mod + 4`.
     if (this.abilities) {
-      for (let [k,v] of Object.entries(this.abilities)) {
+      for (let [k, v] of Object.entries(this.abilities)) {
         data[k] = foundry.utils.deepClone(v);
       }
     }
 
     data.lvl = this.attributes.level.value;
 
-    return data
+    return data;
   }
 }
